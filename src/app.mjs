5116a832dc54ecffe31377465803b02f876b94fa
Daria Steblovska <darias1896.96@gmail.com>
2025-11-12 01:39:44 +0000

    error handler for non-existent routes added to app

--- src/app.mjs
+++ src/app.mjs
@@ -1,9 +1,17 @@
 import express from "express";
 import routes from "./routes/index.mjs";
+import errorController from "./controllers/errorController.mjs";
+import AppError from "./utils/AppError.mjs";
 
 const app = express();
 
 app.use(express.json())
 app.use('/api', routes)
+
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404))
+})
+
+app.use(errorController)
 
 export default app
