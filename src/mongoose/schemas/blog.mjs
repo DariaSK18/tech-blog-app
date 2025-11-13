bb4f92419627d3211d2eeb4756f2a49d7f07ced9
Daria Steblovska <darias1896.96@gmail.com>
2025-11-13 09:47:09 +0000

    blog mongoose schema created

---
+++ src/mongoose/schemas/blog.mjs
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const BlogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: mongoose.Schema.Types.String,
+      required: [true, "Title is required"],
+      trim: true,
+    },
+    content: {
+      type: mongoose.Schema.Types.String,
+      required: [true, "Content is required"],
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Author is required"],
+    },
+    tags: {
+        type: mongoose.Schema.Types.String,
+        default: []
+    }
+  },
+  {
+    timestamp: true,
+  }
+);
+
+export const Blog = mongoose.model("Blog", BlogSchema);
