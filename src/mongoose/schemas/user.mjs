e98f0a263a9f8b0db6d466d38efb594e7573a457
Daria Steblovska <darias1896.96@gmail.com>
2025-11-13 09:44:15 +0000

    user mongoose schema created

---
+++ src/mongoose/schemas/user.mjs
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: mongoose.Schema.Types.String,
+      required: [true, "Username is required"],
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: mongoose.Schema.Types.String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: mongoose.Schema.Types.String,
+      required: [true, "Password is required"],
+      minlength: 6,
+    },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+export const User = mongoose.model("User", UserSchema);
