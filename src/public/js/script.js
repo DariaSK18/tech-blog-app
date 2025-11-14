4936a1426593409ccce2b9c5b4b839407680070d
Daria Steblovska <darias1896.96@gmail.com>
2025-11-14 21:40:26 +0000

    registration func created

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -19,6 +19,43 @@ function showMessage(text, type, duration = 3000) {
 }
 
 // --- registration ---
+if(registerForm) {
+    registerForm.addEventListener('submit', async (e) => {
+        e.preventDefault()
+
+        const username = registerForm.username.value.trim()
+        const email = registerForm.email.value.trim()
+        const password = registerForm.password.value.trim()
+        const confirmation = registerForm.confirmation.value.trim()
+
+        if(!username || !email || !password || !confirmation) {
+            showMessage('All fields are required', 'error')
+            return
+        }
+        if(password !== confirmation) {
+            showMessage('Passwords don\'t match', 'error')
+            return
+        }
+        try {
+            const res = await fetch('/api/user', {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({username, email, password})
+            })
+            if (res.ok) {
+                showMessage("Registration successful!", "success")
+                setTimeout(()=>{
+                    window.location.href = '/login'
+                }, 300)
+            }else {
+        showMessage("Registration failed", "error");
+      }
+        } catch (error) {
+            console.log(error);
+            
+        }
+    })
+}
 
 // --- login ---
 if (loginForm) {
