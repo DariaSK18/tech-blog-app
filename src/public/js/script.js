e1dc1a24794454df6c9584a509a2ac7a5740ef12
Daria Steblovska <darias1896.96@gmail.com>
2025-11-14 19:41:55 +0000

    login func created

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -21,6 +21,24 @@ function showMessage(text, type, duration = 3000) {
 // --- registration ---
 
 // --- login ---
+if (loginForm) {
+  loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const username = loginForm.username.value.trim();
+    // const email = loginForm.email.value.trim();
+    const password = loginForm.password.value.trim();
+
+    const res = await fetch("/api/user/auth", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    });
+    if (res.ok) {
+      window.location.href = "/api/blog";
+    } else showMessage("Invalid username or password", "error");
+  });
+}
 
 // --- logout ---
 logoutBtns.forEach((btn) => {
