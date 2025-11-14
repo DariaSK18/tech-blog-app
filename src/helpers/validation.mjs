18f1a6a497267db22a0324eefd77c29f2c5fd7d6
Daria Steblovska <darias1896.96@gmail.com>
2025-11-14 02:22:14 +0000

    validation schema added for login user

--- src/helpers/validation.mjs
+++ src/helpers/validation.mjs
@@ -179,3 +179,37 @@ export const blogPatch = {
     trim: true,
   },
 };
+
+export const userLogin = {
+  username: {
+    bail: true,
+    exists: { errorMessage: "Username is required" },
+    isString: { errorMessage: "Must be a string" },
+    notEmpty: { errorMessage: "Must be not empty" },
+    isLength: {
+      options: {
+        min: 3,
+        max: 20,
+      },
+      errorMessage: "Must be at least 3-20 characters",
+    },
+    stripLow: true,
+    trim: true,
+  },
+  password: {
+    bail: true,
+    exists: { errorMessage: "Password is required" },
+    isString: { errorMessage: "Must be a string" },
+    notEmpty: { errorMessage: "Must be not empty" },
+    isLength: {
+      options: { min: 6 },
+      errorMessage: "Must be at least 6 characters",
+    },
+    matches: {
+      options: /^(?=.*[A-Za-z])(?=.*\d).+$/,
+      errorMessage: "Password must contain at least one letter and one number",
+    },
+    trim: true,
+    stripLow: true,
+  },
+};
