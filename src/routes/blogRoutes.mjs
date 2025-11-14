32f4146a1315409d0cde10b5ef4fa34a9b6cde22
Daria Steblovska <darias1896.96@gmail.com>
2025-11-14 02:13:57 +0000

    blog routes updated (validation result middleware added)

--- src/routes/blogRoutes.mjs
+++ src/routes/blogRoutes.mjs
@@ -3,18 +3,19 @@ import * as blogController from "../controllers/blogController.mjs";
 import { isUser, isAuth, isAuthor } from "../middleware/auth.mjs";
 import { checkSchema } from "express-validator";
 import { blogValidation, blogPatch } from "../helpers/validation.mjs";
+import { validate } from "../middleware/validate.mjs";
 
 const router = Router();
 
 router
   .route("/")
   .get(blogController.getAllBlogs)
-  .post(isAuth, checkSchema(blogValidation), blogController.createBlog);
+  .post(isAuth, checkSchema(blogValidation), validate, blogController.createBlog);
 
 router
   .route("/:id")
   .get(blogController.getOneBlog)
-  .patch(isAuth, isAuthor, checkSchema(blogPatch), blogController.updateBlog)
+  .patch(isAuth, isAuthor, checkSchema(blogPatch), validate, blogController.updateBlog)
   .delete(isAuth, isAuthor, blogController.deleteBlog);
 
 export default router;
