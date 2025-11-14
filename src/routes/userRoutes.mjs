e9231e3ea6b3cea979a6957b8bcaba6c2701fa6f
Daria Steblovska <darias1896.96@gmail.com>
2025-11-14 02:18:48 +0000

    middlewares added to user routes (validation schemas)

--- src/routes/userRoutes.mjs
+++ src/routes/userRoutes.mjs
@@ -1,26 +1,25 @@
 import { Router } from "express";
 import * as userController from "../controllers/userController.mjs";
 import { isAuth } from "../middleware/auth.mjs";
+import { validate } from "../middleware/validate.mjs";
+import { userPatch, signupValidation, userLogin } from "../helpers/validation.mjs";
+import { checkSchema } from "express-validator";
 
 const router = Router();
 
 router
   .route("/")
   .get(userController.getAllUsers)
-  .post(userController.createUser);
+  .post(checkSchema(signupValidation), validate, userController.createUser);
 
 router
   .route("/:id")
   .get(userController.getOneUser)
-  .patch(isAuth, userController.updateUser)
+  .patch(isAuth, checkSchema(userPatch), validate, userController.updateUser)
   .delete(isAuth, userController.deleteUser);
 
-router
-  .route("/auth")
-  .post(userController.loginUser)
+router.route("/auth").post(checkSchema(userLogin), validate, userController.loginUser);
 
-router
-  .route("/logout")
-  .post(isAuth, userController.logoutUser)
+router.route("/logout").post(isAuth, userController.logoutUser);
 
 export default router;
