eb42705d97cff5872206312ed9c562fdac265f1b
Daria Steblovska <darias1896.96@gmail.com>
2025-11-14 02:07:20 +0000

    blog routes updated (middlewares added)

--- src/routes/blogRoutes.mjs
+++ src/routes/blogRoutes.mjs
@@ -1,18 +1,20 @@
 import { Router } from "express";
 import * as blogController from "../controllers/blogController.mjs";
-import { isUser } from "../middleware/auth.mjs";
+import { isUser, isAuth, isAuthor } from "../middleware/auth.mjs";
+import { checkSchema } from "express-validator";
+import { blogValidation, blogPatch } from "../helpers/validation.mjs";
 
 const router = Router();
 
 router
   .route("/")
   .get(blogController.getAllBlogs)
-  .post(isUser, blogController.createBlog);
+  .post(isAuth, checkSchema(blogValidation), blogController.createBlog);
 
 router
   .route("/:id")
   .get(blogController.getOneBlog)
-  .patch(blogController.updateBlog)
-  .delete(blogController.deleteBlog);
+  .patch(isAuth, isAuthor, checkSchema(blogPatch), blogController.updateBlog)
+  .delete(isAuth, isAuthor, blogController.deleteBlog);
 
 export default router;
