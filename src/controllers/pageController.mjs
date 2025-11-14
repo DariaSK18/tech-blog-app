acb9dd18f71c6f4bee360a0efbc51a24cc1c9a22
Daria Steblovska <darias1896.96@gmail.com>
2025-11-14 19:04:52 +0000

    controllers created for pages

--- src/controllers/pageController.mjs
+++ src/controllers/pageController.mjs
@@ -1,12 +1,22 @@
 export const homePage = (req, res) => {
-    res.render('index', {title: 'Home'})
-}
+  res.render("index", { title: "Home" });
+};
 export const loginPage = (req, res) => {
-    res.render('login', {title: 'Login'})
-}
+  res.render("login", { title: "Login" });
+};
 export const registerPage = (req, res) => {
-    res.render('register', {title: 'Register'})
-}
+  res.render("register", { title: "Register" });
+};
 export const profilePage = (req, res) => {
-    res.render('profile', {title: 'Profile'})
-}
+  res.render("profile", { title: "Profile" });
+};
+
+export const blogsPage = (req, res) => {
+  res.render("blogs", { title: "Blogs" });
+};
+export const changePswPage = (req, res) => {
+  res.render("change-psw", { title: "Edit" });
+};
+export const writeBlogPage = (req, res) => {
+  res.render("write-blog", { title: "Write blog" });
+};
