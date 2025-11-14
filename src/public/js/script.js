14bdce66d5e52ce7541ec98f692e0fbf66d11900
Daria Steblovska <darias1896.96@gmail.com>
2025-11-14 19:14:54 +0000

    show message func created

---
+++ src/public/js/script.js
@@ -0,0 +1,19 @@
+// --- forms and btns ---
+const registerForm = document.getElementById("registerForm");
+const loginForm = document.getElementById("loginForm");
+const blogForm = document.getElementById("blogForm");
+const logoutBtns = document.querySelectorAll(".logout");
+const blogsList = document.querySelector(".blogs-list");
+
+// --- show message ---
+function showMessage(text, type, duration = 3000) {
+  const msg = document.getElementById("message");
+  if (!msg) return;
+
+  msg.innerText = text;
+  msg.className = `message show ${type}`;
+
+  setTimeout(() => {
+    msg.classList.remove("show");
+  }, duration);
+}
