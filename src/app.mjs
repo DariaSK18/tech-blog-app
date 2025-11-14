7002f9536a0545d5d654674b5be41dba4ef1bf61
Daria Steblovska <darias1896.96@gmail.com>
2025-11-14 22:58:01 +0000

    bug fixed with connecting fronend

--- src/app.mjs
+++ src/app.mjs
@@ -24,7 +24,7 @@ mongoose
 
 app.set('views', './src/views')
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static("src/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
