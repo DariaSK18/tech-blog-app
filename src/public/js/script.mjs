36d8daf4f9385ae6b0efe0fad57ecf444f73dad9
Daria Steblovska <darias1896.96@gmail.com>
2025-11-15 05:29:13 +0000

    edit or delete blog func

--- src/public/js/script.mjs
+++ src/public/js/script.mjs
@@ -125,6 +125,33 @@ if (blogForm) {
 }
 
 // --- delete / edit blog ---
+if(blogsList) {[
+  blogsList.addEventListener("click", async (e) => {
+    const item = e.target.closest('li')
+    if(!item) return
+
+    const id = item.getAttribute('id')
+
+    if(e.target.classList.contains('deleteBlog')) {
+      const ok = confirm("Delete blog?")
+      if(!ok) return
+    }
+    const res = await fetch(`/api/blog/${id}`, {
+      method: "DELETE"
+    })
+
+    if(res.ok) {
+      showMessage('Deleted', "success")
+      setTimeout(() => {
+        location.reload()
+      }, 300)
+    }
+
+    if(e.target.classList.contains('editBlog')) {
+      window.location.href = `/write-blog/${id}`
+    }
+  })
+]}
 
 // --- change psw ---
 
