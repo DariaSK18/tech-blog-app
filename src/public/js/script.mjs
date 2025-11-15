f89c2613a09a9057619e49ac36812db212b9c611
Daria Steblovska <darias1896.96@gmail.com>
2025-11-15 09:44:57 +0000

    imports added to main js file

--- src/public/js/script.mjs
+++ src/public/js/script.mjs
@@ -1,1 +1,17 @@
+// --- import auth files ---
+import "./auth/login.mjs"
+import "./auth/logout.mjs"
+import "./auth/register.mjs"
 
+// --- import blog files ---
+import "./blogs/blogForm.mjs"
+import "./blogs/blogList.mjs"
+import "./blogs/search.mjs"
+
+// --- import profile files ---
+import "./profile/changePassword.mjs"
+import "./profile/deleteProfile.mjs"
+
+// --- import utils files ---
+import "./utils/passwordToggle.mjs"
+import "./utils/showMessage.mjs"
