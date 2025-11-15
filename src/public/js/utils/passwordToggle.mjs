2ff51198127e8f1e201681eb5ea9655da7f41f51
Daria Steblovska <darias1896.96@gmail.com>
2025-11-15 09:42:07 +0000

    utils files toggle psw

---
+++ src/public/js/utils/passwordToggle.mjs
@@ -0,0 +1,25 @@
+
+const passwordFields = document.querySelectorAll('input[type="password"]');
+
+// --- eye for psw ---
+
+if (passwordFields) {
+  passwordFields.forEach((input) => {
+    const toggleBtn = document.createElement("button");
+    toggleBtn.type = "button";
+    toggleBtn.innerText = "show";
+    toggleBtn.classList.add("toggle-password");
+
+    input.insertAdjacentElement("afterend", toggleBtn);
+
+    toggleBtn.addEventListener("click", () => {
+      if (input.type === "password") {
+        input.type = "text";
+        toggleBtn.innerText = "hide";
+      } else {
+        input.type = "password";
+        toggleBtn.innerText = "show";
+      }
+    });
+  });
+}
